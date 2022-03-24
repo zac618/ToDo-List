7a4c8f7872ba84d5bf97b8ba12bebdd5858a7d80
Zac E <zacegleston@gmail.com>
2022-03-24 18:16:29 -0500

    Needs delete buttons fixed
    I need to figure out how to make delete buttons with each new li and then make them delete their li when clicked.

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 let todoArray = [];
 
+
 document.getElementById('add_button').addEventListener('click', function() {
 	let list = document.getElementById('myUL');
+	let li = document.createElement("li");
 	let todoInput = document.getElementById('myInput').value;
-	todoArray.push(todoInput);
+	// li.setAttribute("id", "new-li");
+	li.appendChild(document.createTextNode(todoInput));
+	list.appendChild(li);
 
-	let item = document.createElement('li');
-	for (var i = 0; i < todoArray.length; i++) {
-		var listItem = document.createTextNode(todoArray[i]);
-		list.append(document.createTextNode(todoArray[i]));
+	li.innerHTML +=
+	`<button onclick="del(todoInput)">Del</button>`;
+		
+	document.getElementById('myInput').value = "";
 	}
-});
 
-// So far, I like the below option the best
 
-// function addingTask() {
-// 	let inputText = document.getElementById('myInput').value;
-// 	todoArray.push(inputText);
-// }
 
-// function printTodos() {
-// 	let list = document.getElementById('myUL');
-// 	// list.innerHTML = ''; // Reset the list content whenever we print the todos
-
-// 	for (let i = 0; i < todoArray.length; i++) {
-// 		let li = document.createElement('li');
-// 		let listItem = li.appendChild(document.createTextNode(todoArray[i]));
-// 		list.appendChild(listItem);
-// 	}
-// }
-
-// document.getElementById('add_button').addEventListener('click', function() {
-// 	addingTask();
-// 	printTodos();
-// });
-
-// console.log(todoArray);
+);
