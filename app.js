d4045af7438b6ec9cb75b538728c646a3b5311f2
Zac E <zacegleston@gmail.com>
2022-03-26 19:49:49 -0500

    Still struggling to delete items

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ document.getElementById('add_button').addEventListener('click', function() {
 	let li = document.createElement("li");
 	let todoInput = document.getElementById('myInput').value;
 	// li.setAttribute("id", "new-li");
+	let button = document.createElement("deleteButton");
+	button.innerHTML = "Delete";
+	li.appendChild(button);
 	li.appendChild(document.createTextNode(todoInput));
 	list.appendChild(li);
 
-	li.innerHTML +=
-	`<button onclick="del(todoInput)">Del</button>`;
+
+	// li.innerHTML +=
+	// `<button onclick="del(todoInput)">Del</button>`;
 		
 	document.getElementById('myInput').value = "";
 	}
-
-
-
 );
+
+document.getElementById('deleteButton').addEventListener('click', function() {
+	let list = document.getElementById('myUL');
+	let li = document.getElementById('li');
+	removeChild(list);
+})
+
+// document.getElementById('deleteButton').addEventListener('click', function() {
+// 	deleteButton.parentElement.remove();
+// 	return;
+// });
