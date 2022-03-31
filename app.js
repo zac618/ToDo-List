ccd7158f5f28adb5a2917cf87ff3a60041136113
Zac E <zacegleston@gmail.com>
2022-03-30 21:21:25 -0500

    Finished ToDo List

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-let todoArray = [];
 
 
-document.getElementById('add_button').addEventListener('click', function() {
+document.getElementById('add_button').addEventListener('click', addItem);
+document.getElementById('myInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      addItem();
+    }
+});
+
+
+function addItem() {
+	// Get the list
 	let list = document.getElementById('myUL');
+	let input = document.getElementById('myInput');
+	
+	// Create delete button with li
+	let deleteButton = document.createElement("button");
+	deleteButton.setAttribute("class", "dltBtn");
+	deleteButton.innerText = "Delete";
+
+	// Create Edit button with li
+	let editButton = document.createElement("button");
+	editButton.setAttribute("class", "editBtn");
+	editButton.innerText = "Edit";
+
+	// Declare variable to use 
 	let li = document.createElement("li");
-	let todoInput = document.getElementById('myInput').value;
-	// li.setAttribute("id", "new-li");
-	let button = document.createElement("deleteButton");
-	button.innerHTML = "Delete";
-	li.appendChild(button);
-	li.appendChild(document.createTextNode(todoInput));
-	list.appendChild(li);
 
+	
+	// For loop to go through list of items
+	//for (let i = 0; i < todoArray.length; i++);
 
-	// li.innerHTML +=
-	// `<button onclick="del(todoInput)">Del</button>`;
+		// Append the added word
+		const text = document.createElement("p")
+		text.innerText = input.value;
+		li.appendChild(text);
 		
-	document.getElementById('myInput').value = "";
+		// Reset input box to be blank after adding item
+		input.value = "";
+
+		// Add to list
+		list.appendChild(li);
+		li.appendChild(deleteButton).addEventListener("click", removeItem);
+		li.appendChild(editButton).addEventListener("click", editItem);
+
+}
+
+// Delete Item
+function removeItem(event) {
+	event.target.parentElement.remove()
+}
+
+// Edit Item
+function editItem(event) {
+	let updatedItem = prompt('What would you like this to say?');
+	let $li = event.target.parentElement
+	if (updatedItem) {
+		$li.querySelector('p').innerText = updatedItem;
 	}
-);
-
-document.getElementById('deleteButton').addEventListener('click', function() {
-	let list = document.getElementById('myUL');
-	let li = document.getElementById('li');
-	removeChild(list);
-})
-
-// document.getElementById('deleteButton').addEventListener('click', function() {
-// 	deleteButton.parentElement.remove();
-// 	return;
-// });
+	console.log(updatedItem)
+	
+}
